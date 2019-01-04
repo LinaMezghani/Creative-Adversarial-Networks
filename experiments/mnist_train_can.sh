ba01664eddca3463eb00ffbffbc4b699a2d30d38
Lina Mezghani <35572460+LinaMezghani@users.noreply.github.com>
2019-01-04 17:40:00 +0100

    Update mnist_train_can.sh

--- experiments/mnist_train_can.sh
+++ experiments/mnist_train_can.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=2
 python3 main.py \
 --epoch 25 \
 --learning_rate .0001 \
@@ -11,7 +11,7 @@ python3 main.py \
 --smoothing 1 \
 --dataset mnist \
 --input_fname_pattern */*.jpg \
---checkpoint_dir checkpoint \
+--checkpoint_dir checkpoint2 \
 --sample_dir samples \
 --crop False \
 --visualize False \
