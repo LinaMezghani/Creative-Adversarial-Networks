7b1615ead63638472357fd00e426729fad2dcc12
Lina Mezghani <35572460+LinaMezghani@users.noreply.github.com>
2019-01-05 16:33:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -137,7 +137,6 @@ def main(_):
 
   run_config = tf.ConfigProto()
   run_config.gpu_options.allow_growth=FLAGS.allow_gpu_growth
-  
   with tf.Session(config=run_config) as sess:
     dcgan.set_sess(sess)
     # show_all_variables()
