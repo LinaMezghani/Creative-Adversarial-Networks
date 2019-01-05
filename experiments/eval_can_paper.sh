cff99ee2dc7ffffd9e92319d835bc7856629749b
Lina Mezghani <35572460+LinaMezghani@users.noreply.github.com>
2019-01-05 15:47:51 +0100

    Update eval_can_paper.sh

--- experiments/eval_can_paper.sh
+++ experiments/eval_can_paper.sh
@@ -1,5 +1,5 @@
 # export CUDA_VISIBLE_DEVICES=0 # edit this if you want to limit yourself to GPU
-export PYTHONPATH="slim/:$PYTHONPATH"
+#export PYTHONPATH="slim/:$PYTHONPATH"
 python3 main.py \
 --epoch 25 \
 --learning_rate .0001 \
@@ -10,11 +10,12 @@ python3 main.py \
 --output_height 256 \
 --lambda_val 1.0 \
 --smoothing 1.0 \
---use_resize True \
+--use_resize=True \
 --dataset wikiart \
 --input_fname_pattern */*.jpg \
 --load_dir "logs/can_paper"
---crop False \
---visualize False \
---can True \
+--crop=False \
+--visualize=False \
+--can=True \
+--wgan=False \
 --train False
