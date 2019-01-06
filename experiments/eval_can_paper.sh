f7288b79c458977865205639d3f9ab34776715e7
Lina Mezghani <35572460+LinaMezghani@users.noreply.github.com>
2019-01-06 13:19:45 +0100

    Update eval_can_paper.sh

--- experiments/eval_can_paper.sh
+++ experiments/eval_can_paper.sh
@@ -6,14 +6,14 @@ python3 main.py \
 --beta 0.5 \
 --batch_size 16 \
 --sample_size 16 \
---input_height 256 \
---output_height 256 \
+--input_height 64 \
+--output_height 64 \
 --lambda_val 1.0 \
 --smoothing 1.0 \
 --use_resize=True \
 --dataset wikiart \
 --input_fname_pattern */*.jpg \
---checkpoint_dir checkpoint \
+--checkpoint_dir = \
 --crop=False \
 --visualize=False \
 --can=True \
